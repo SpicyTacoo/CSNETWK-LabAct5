c645323434cd3372b3a57acb06f8589d68a93655
Taco <bensonyao123@gmail.com>
2023-11-05 03:29:44 +0800

    forgot to save

--- CSNETWK-WebServer.py
+++ CSNETWK-WebServer.py
@@ -19,22 +19,25 @@ while True:
         outputdata = #Fill in start #Fill in end
         #Send one HTTP header line into socket
         #Fill in start
+        connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
         #Fill in end
         #Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
             connectionSocket.send(outputdata[i].encode())
-       
+        
         connectionSocket.send("\r\n".encode())
         connectionSocket.close()
     
     except IOError:
         #Send response message for file not found
         #Fill in start
+        connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
         #Fill in end
-       
+        
         #Close client socket
         #Fill in start
-        
+        connectionSocket.close()
         #Fill in end
 serverSocket.close()
 sys.exit()  #Terminate the program after sending the corresponding data
+
